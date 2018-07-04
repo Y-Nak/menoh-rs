b1f56235cd64db01f952433f04d8f8218f7ed0e9
Y-Nak <yurayura.rounin.3@gmail.com>
2018-07-04 18:23:25 +0900

    Improve interface of model_data

--- src/model_data.rs
+++ src/model_data.rs
@@ -1,8 +1,8 @@
 //! This module contains ModelData
 //!
 //!
+use std;
 use std::ffi::CString;
-use std::mem;
 use std::path::Path;
 
 use error::{cvt_r, Error};
@@ -15,9 +15,12 @@ pub struct ModelData {
 
 impl ModelData {
     /// Create ModelData from given ONNX file.
-    pub fn new(onnx_path: &Path) -> Result<Self, Error> {
-        let mut handle: menoh_model_data_handle = unsafe { mem::uninitialized() };
-        let p = CString::new(onnx_path.to_str().ok_or(Error::InvalidFileName)?)
+    pub fn new<P>(onnx_path: &P) -> Result<Self, Error>
+    where
+        P: AsRef<Path>,
+    {
+        let mut handle: menoh_model_data_handle = std::ptr::null_mut();
+        let p = CString::new(onnx_path.as_ref().to_str().ok_or(Error::InvalidFileName)?)
             .map_err(|_| Error::InvalidFileName)?;
         cvt_r(|| unsafe {
             ffi::menoh_make_model_data_from_onnx(
