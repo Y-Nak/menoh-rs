e1fde4c4dc22e8d34d3daf5e32d33fede65af00f
Y-Nak <yurayura.rounin.3@gmail.com>
2018-07-04 18:22:43 +0900

    Expose structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ extern crate libc;
 pub mod ffi;
 
 pub mod error;
+pub mod model;
 pub mod model_data;
 pub mod variable_profile;
-pub mod model;
+
+pub use model::{Model, ModelBuilder};
+pub use model_data::ModelData;
+pub use variable_profile::{Dtype, VariableProfileTable, VariableProfileTableBuilder};
