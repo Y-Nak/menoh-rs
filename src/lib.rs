5073cef279707e172533c173ed9dc5e02ad10196
Y-Nak <yurayura.rounin.3@gmail.com>
2018-07-04 20:32:32 +0900

    Add dtype compatible trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ extern crate libc;
 
 pub mod ffi;
 
+pub mod dtype;
 pub mod error;
 pub mod model;
 pub mod model_data;
 pub mod variable_profile;
 
+pub use dtype::Dtype;
 pub use model::{Model, ModelBuilder};
 pub use model_data::ModelData;
-pub use variable_profile::{Dtype, VariableProfileTable, VariableProfileTableBuilder};
+pub use variable_profile::{VariableProfileTable, VariableProfileTableBuilder};
