057e3a29fa9a6dda810a6abf53ce2930576c48e4
Y-Nak <yurayura.rounin.3@gmail.com>
2018-07-05 09:04:41 +0900

    Comment work

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@
 //!
 //! let mut model_builder = mehoh::ModelBuilder::new(&vpt).unwrap();
 //! model_builder
-//!     .attach_external_buffer(CONV1_1_IN_NAME, &buffer)
+//!     .attach_external_buffer(CONV1_1_IN_NAME, &buffer, &vpt)
 //!     .unwrap();
 //!
 //! let mut model = model_builder
